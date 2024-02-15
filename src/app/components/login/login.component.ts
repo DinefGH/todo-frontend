5858d92caacc59ff317de2fa26ab70d9b3343c6e
DinefGH <fabianduerr@web.de>
2024-02-15 05:14:41 +0100

    login function

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -6,10 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-email: string = '';
-password: string = ''
+username: string = '';
+password: string = '';
 
-login() {
-  // Logik um mit Backend zu kommunizieren
+
+
+async login() {
+  const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const raw = JSON.stringify({
+  "username": this.username,
+  "password": this.password
+});
+
+const requestOptions:RequestInit = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
+try {
+let resp = await fetch("http://127.0.0.1:8000/login/", requestOptions)
+let json = await resp.json();
+localStorage.setItem('token', json.token);
+} catch (e) {
+console.error(e);
+}
 }
 }
